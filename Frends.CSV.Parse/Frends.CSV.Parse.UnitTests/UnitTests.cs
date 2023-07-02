665586bb6c448bd674bd33bbcb5facff83f7a215
akankainen <ari.kankainen@hiq.fi>
2023-07-02 14:11:55 +0300

    Issue11: Adds possibility to treat missing fields as nulls instead of throwing a MissingFieldException

--- Frends.CSV.Parse/Frends.CSV.Parse.UnitTests/UnitTests.cs
+++ Frends.CSV.Parse/Frends.CSV.Parse.UnitTests/UnitTests.cs
@@ -1,6 +1,7 @@
 using Frends.CSV.Parse.Definitions;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Newtonsoft.Json.Linq;
+using System.ComponentModel.DataAnnotations;
 
 namespace Frends.CSV.Parse.UnitTests;
 
@@ -165,5 +166,171 @@ year;car;mark;price
         Assert.AreEqual('f', itemArray[6]);
         Assert.AreEqual(typeof(DateTime), itemArray[7].GetType());
         Assert.AreEqual(typeof(DateTime), itemArray[8].GetType());
+    }
+
+    [TestMethod]
+    public void TestParseTreatMissingFieldsAsNullSetToTrue()
+    {
+        var csv =
+            @"header1,header2,header3
+                value1,value2,value3
+                value1,value2,value3
+                value1,value2";
+
+        var input = new Input()
+        {
+            ColumnSpecifications = new ColumnSpecification[0],
+            Delimiter = ",",
+            Csv = csv
+        };
+
+        var options = new Options()
+        {
+            ContainsHeaderRow = true,
+            CultureInfo = "fi-FI",
+            TreatMissingFieldsAsNulls = true
+        };
+
+        var result = CSV.Parse(input, options, default);
+
+        var resultJson = (JArray)result.Jtoken;
+        Assert.AreEqual(resultJson[2].Value<string>("header3"), null);
+
+        var resultXml = result.Xml;
+        Assert.IsTrue(condition: resultXml.ToString()?.Contains("<header3 />"));
+
+        var resultData = result.Data;
+        var nullItem = resultData[2][2];
+
+        Assert.AreEqual(nullItem, null);
+    }
+
+    [TestMethod]
+    public void TestParseTreatMissingFieldsAsNullSetToTrueNoHeader()
+    {
+        var csv =
+            @"value1,value2,value3
+              value1,value2,value3
+              value1,value2";
+
+        var input = new Input()
+        {
+            ColumnSpecifications = new ColumnSpecification[0],
+            Delimiter = ",",
+            Csv = csv
+        };
+
+        var options = new Options()
+        {
+            ContainsHeaderRow = false,
+            CultureInfo = "fi-FI",
+            TreatMissingFieldsAsNulls = true
+        };
+
+        var result = CSV.Parse(input, options, default);
+
+        var resultJson = (JArray)result.Jtoken;
+        Assert.AreEqual(resultJson[2].Value<string>("2"), null);
+
+        var resultXml = result.Xml;
+        Assert.IsTrue(condition: resultXml.ToString()?.Contains("<2 />"));
+
+        var resultData = result.Data;
+        var nullItem = resultData[2][2];
+
+        Assert.AreEqual(nullItem, null);
+    }
+
+    [TestMethod]
+    public void TestParseTreatMissingFieldsAsNullSetToTrueNoHeaderWithColumnSpecifications()
+    {
+        var csv =
+            @"string,1,2023-01-01,2,200,3,true,N
+              ,,,,,,,";
+
+        var input = new Input()
+        {
+            ColumnSpecifications = new[]
+            {
+                new ColumnSpecification() { Name = "String", Type = ColumnType.String },
+                new ColumnSpecification() { Name = "Decimal", Type = ColumnType.Decimal},
+                new ColumnSpecification() { Name = "DateTime", Type = ColumnType.DateTime },
+                new ColumnSpecification() { Name = "Int", Type = ColumnType.Int },
+                new ColumnSpecification() { Name = "Long", Type = ColumnType.Long },
+                new ColumnSpecification() { Name = "Double", Type = ColumnType.Double },
+                new ColumnSpecification() { Name = "Boolean", Type = ColumnType.Boolean },
+                new ColumnSpecification() { Name = "Char", Type = ColumnType.Char }
+            },
+            Delimiter = ",",
+            Csv = csv
+        };
+
+        var options = new Options()
+        {
+            ContainsHeaderRow = false,
+            CultureInfo = "fi-FI",
+            TreatMissingFieldsAsNulls = true
+        };
+
+        // The boxed type cannot be checked against Nullable<T> so to make sure no exception gets
+        // thrown when TreatMissingFieldsAsNulls is set to true have to check if exception is thrown
+        // when trying to insert empty values in the data
+        try
+        {
+            var result = CSV.Parse(input, options, default);
+            var resultData = result.Data;
+            var itemArray = resultData[1];
+
+            Assert.IsNull(itemArray[1]);
+            Assert.IsNull(itemArray[2]);
+            Assert.IsNull(itemArray[3]);
+            Assert.IsNull(itemArray[4]);
+            Assert.IsNull(itemArray[5]);
+            Assert.IsNull(itemArray[6]);
+            Assert.IsNull(itemArray[7]);
+        }
+        catch (Exception ex)
+        {
+            Assert.Fail("Should not throw an exception");
+        }
+    }
+
+    [TestMethod]
+    [ExpectedException(typeof(CsvHelper.MissingFieldException))]
+    public void TestParseTreatMissingFieldsAsNullSetToFalse()
+    {
+        var csv =
+            @"header1,header2,header3
+                value1,value2,value3
+                value1,value2,value3
+                value1,value2";
+        var input = new Input()
+        {
+            ColumnSpecifications = new ColumnSpecification[0],
+            Delimiter = ",",
+            Csv = csv
+        };
+
+        var options = new Options()
+        {
+            ContainsHeaderRow = true,
+            CultureInfo = "fi-FI",
+            TreatMissingFieldsAsNulls = false
+        };
+
+        var result = CSV.Parse(input, options, default);
+    }
+
+    [TestMethod]
+    public void TestParseTreatMissingFieldsAsNullDefaultValue()
+    {
+        var options = new Options();
+
+        Assert.AreEqual(options.TreatMissingFieldsAsNulls, false);
+    }
+
+    public bool IsNullable<T>(T value)
+    {
+        return Nullable.GetUnderlyingType(typeof(T)) != null;
     }
 }
--- Frends.CSV.Parse/Frends.CSV.Parse/Definitions/Options.cs
+++ Frends.CSV.Parse/Frends.CSV.Parse/Definitions/Options.cs
@@ -29,7 +29,7 @@ public class Options
     public int SkipRowsFromTop { get; set; }
 
     /// <summary>
-    /// A flag to let the reader know if a record should be skipped when reading if it's empty. 
+    /// A flag to let the reader know if a record should be skipped when reading if it's empty.
     /// A record is considered empty if all fields are empty.
     /// </summary>
     /// <example>true</example>
@@ -45,11 +45,18 @@ public class Options
     public string ReplaceHeaderWhitespaceWith { get; set; }
 
     /// <summary>
-    /// The culture info to read/write the entries with, e.g. for decimal separators. 
-    /// InvariantCulture will be used by default. 
+    /// The culture info to read/write the entries with, e.g. for decimal separators.
+    /// InvariantCulture will be used by default.
     /// See list of cultures here: https://learn.microsoft.com/en-us/dotnet/api/system.globalization.cultureinfo.name?view=net-7.0
     /// NOTE: Due to an issue with the CsvHelpers library, all CSV tasks will use the culture info setting of the first CSV task in the process; you cannot use different cultures for reading and parsing CSV files in the same process.|
     /// </summary>
     /// <example>fi-FI</example>
     public string CultureInfo { get; set; } = "";
+
+    /// <summary>
+    /// The flag for reader to treat missing fields as nulls instead of throwing a MissingFieldException.
+    /// In case of providing column specifications manually the value of empty field defined as string will be empty instead of null
+    /// </summary>
+    [DefaultValue("false")]
+    public bool TreatMissingFieldsAsNulls { get; set; } = false;
 }
