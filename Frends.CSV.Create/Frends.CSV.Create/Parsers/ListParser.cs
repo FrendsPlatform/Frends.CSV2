1ad1f583cfe9931311ef25b648e8e5bc179139b8
MatteoDelOmbra <mateusz.noga.wojtania@gmail.com>
2025-10-27 08:23:11 +0100

    split code to files

---
+++ Frends.CSV.Create/Frends.CSV.Create/Parsers/ListParser.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using CsvHelper;
+using CsvHelper.Configuration;
+using Frends.CSV.Create.Definitions;
+
+namespace Frends.CSV.Create.Parsers;
+
+internal static class ListParser
+{
+    internal static string ListToCsvString(List<List<object>> inputData, List<string> inputHeaders,
+        CsvConfiguration config, Options options, CancellationToken cancellationToken)
+    {
+        using var csvString = new StringWriter();
+        using var csv = new CsvWriter(csvString, config);
+
+        //Write the header row
+        if (config.HasHeaderRecord && inputData.Any())
+        {
+            foreach (var column in inputHeaders)
+            {
+                cancellationToken.ThrowIfCancellationRequested();
+                csv.WriteField(column);
+            }
+
+            csv.NextRecord();
+        }
+
+        foreach (var row in inputData)
+        {
+            foreach (var cell in row)
+            {
+                cancellationToken.ThrowIfCancellationRequested();
+                csv.WriteField(cell ?? options.ReplaceNullsWith);
+            }
+
+            csv.NextRecord();
+        }
+
+        return csvString.ToString();
+    }
+}
---
+++ Frends.CSV.Create/Frends.CSV.Create/Parsers/XmlParser.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Xml.Linq;
+using CsvHelper;
+using CsvHelper.Configuration;
+using Frends.CSV.Create.Definitions;
+
+namespace Frends.CSV.Create.Parsers;
+
+internal static class XmlParser
+{
+    internal static string XmlToCsvString(string xml, string node, CsvConfiguration config, Options options,
+        CancellationToken cancellationToken)
+    {
+        using var csvString = new StringWriter();
+        using var csv = new CsvWriter(csvString, config);
+
+        XDocument xdoc = XDocument.Parse(xml);
+        IEnumerable<XElement> nodes;
+        if (string.IsNullOrEmpty(node))
+            nodes = xdoc.Root.Elements();
+        else
+            nodes = xdoc.Descendants().Where(p => p.Name.LocalName.Equals(node));
+
+        //Write the header row
+        if (config.HasHeaderRecord && nodes.Any())
+        {
+            var headers = nodes.Descendants().Select(n => n.Name.LocalName).Distinct().ToList();
+
+            foreach (var column in headers)
+            {
+                cancellationToken.ThrowIfCancellationRequested();
+                csv.WriteField(column.ToString());
+            }
+
+            csv.NextRecord();
+        }
+
+        foreach (var column in nodes)
+        {
+            foreach (var cell in column.Elements().Select(n => n.Value).ToList())
+            {
+                cancellationToken.ThrowIfCancellationRequested();
+                csv.WriteField(cell ?? options.ReplaceNullsWith);
+            }
+
+            csv.NextRecord();
+        }
+
+        return csvString.ToString();
+    }
+}
